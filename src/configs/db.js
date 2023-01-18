66aeefb2a106e76da26c403d4b0aeafb4f77c6a4
vikas <vikasnwrdr2001@gmail.com>
2023-01-19 00:43:39 +0530

    deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-const {MONGOURI} = require("./keys")
 
 module.exports = () => {
   return mongoose.connect(
-        MONGOURI
-      // "mongodb+srv://vikas:vikas_123@cluster0.aflw8.mongodb.net/Trek?retryWrites=true&w=majority"
+      "mongodb+srv://vikas:vikas_123@cluster0.aflw8.mongodb.net/Trek?retryWrites=true&w=majority"
     );
 };
 
--- src/configs/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MONGOURI : "mongodb+srv://vikas:vikas_123@cluster0.aflw8.mongodb.net/Trek?retryWrites=true&w=majority"
-}
